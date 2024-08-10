596cd81f91d4f5a580683bf51ee3dbf7b7d7c3de
DenzDy <dydenzellrobyn0527@gmail.com>
2024-08-10 11:45:56 +0800

    Initial commit, learning CsS

---
+++ HelloWorldPlugin/HelloWorldPlugin.cs
@@ -0,0 +1,13 @@
+﻿using CounterStrikeSharp.API.Core;
+namespace HelloWorldPlugin;
+
+public class HelloWorldPlugin : BasePlugin
+{
+    public override string ModuleName => "Hello World Plugin";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override void Load(bool notReload){
+        Console.WriteLine("Hello World!");
+    }
+}
